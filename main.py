f09f0b9ee4e81c3e028ae6983d517997abfa226a
salwoch <>
2022-11-09 06:22:42 +0000

    add counters under collections

--- main.py
+++ main.py
@@ -24,32 +24,33 @@ mow()
 #convert data types
 age = 20
 age = float(age)
-print(age)
+#print(age)
 
 #Arithmetic operators
 # +,-,*,/,%,**,//
 #//-signifies flow division ie rounds something
 x = 5 // 2
-print(x)  #returns 2 to us
+#print(x)  #returns 2 to us
 
 #comparison operators
 a = 20
 b = 40
-print(a == b)  #false
-print(a != b)  #true
-print(a > b)  #false
-print(a <= b)  #true
+
+#print(a == b)  #false
+#print(a != b)  #true
+#print(a > b)  #false
+#print(a <= b)  #true
 
 #boolean operators
 #not,and ,or
 
 #or
 #  returns the first operand that is not a falsey value
-print(0 or 1)  #returns 1
-print(False or "hey")  #returns hey
-print("hi" or "hey")  #returns hi
-print([] or False)  #returns False
-print(False or [])  #returns []
+#print(0 or 1)  #returns 1
+#print(False or "hey")  #returns hey
+#print("hi" or "hey")  #returns hi
+#print([] or False)  #returns False
+#print(False or [])  #returns []
 
 #and
 #evaluates the second argument when the first argument is True
@@ -80,7 +81,7 @@ def is_adult(age):
     return False
 
 
-print(is_adult(20))
+#print(is_adult(20))
 
 
 #with the ternary operator
@@ -88,38 +89,38 @@ def is_adult2(age):
   return True if age > 10 else False
 
 
-print(is_adult2(5))
+#print(is_adult2(5))
 
 #strings
 #characters enclosed in quotation marks
 #strings can be concatenated
 
 #multi-line strings
-print("""sophia is
-  27
-  years old
-  """)
+#print("""sophia is
+# 27
+# years old
+# """)
 
 #string methods
-print("sophia".upper())
-print("sophia".lower())
-print("alwoch sophia".title())
+#print("sophia".upper())
+#print("sophia".lower())
+#print("alwoch sophia".title())
 
 name = "soph"
-print(len(name))
+#print(len(name))
 
 #add special characters to a string using "\"
 # \n splits a string into a new line
 
 #get a specific character in a string
-print(name[3])
+#print(name[3])
 
 #use a negative number to access a string from the end
-print(name[-1])
+#print(name[-1])
 
 #use a range to access certain parts of a string
-print(name[1:3])
-print(name[:3])  #from the beginning to index 2
+#print(name[1:3])
+#print(name[:3])  #from the beginning to index 2
 
 #Booleans (bool type)
 #2 values True and False and they should be capitalised
@@ -132,11 +133,12 @@ print(name[:3])  #from the beginning to index 2
 book1 = True
 book2 = False
 my_books = [book1, book2]
-print(any(my_books))
+
+#print(any(my_books))
 
 #all()
 #returns true if all the members of an iterable are True
-print(any(my_books))
+#print(any(my_books))
 
 #number data Types
 #int
@@ -144,8 +146,8 @@ print(any(my_books))
 #complex
 
 #built in functions that work with numbers
-print(abs(-5.5))  #returns 5.5
-print(round(5.49, 1))  #returns 5.5
+#print(abs(-5.5))  #returns 5.5
+#print(round(5.49, 1))  #returns 5.5
 
 
 #enums
@@ -156,27 +158,27 @@ class State(Enum):
   ACTIVE = 1
 
 
-print(State.ACTIVE)
-print(State.ACTIVE.value)
-print(State(1))
+#print(State.ACTIVE)
+#print(State.ACTIVE.value)
+#print(State(1))
 
 #list all the possible values in an enum
-print(list(State))
-print(len(State))
+#print(list(State))
+#print(len(State))
 
 #lists
 dogs = ["reuben", "roger", "peter"]
 #check if an item is in a list
-print("reuben" in dogs)
+#print("reuben" in dogs)
 #reference items in a list using indices
-print(dogs[2])
+#print(dogs[2])
 #update a member in a list
 dogs[1] = "Carlos"
-print(dogs)
+#print(dogs)
 
 #extract a part of a list
-print(dogs[2:3])
-print(len(dogs))
+#print(dogs[2:3])
+#print(len(dogs))
 
 #add items to a list
 dogs.append('Judah')
@@ -196,27 +198,28 @@ dogs[1:1] = ["Test1", "Test2"]
 #Allow you to create immutable groups of objects ie once a tuple is created, it cannot be modified
 
 people = ("savannah", "phidy")
-print(people[0])
-print(people.index("savannah"))
-print(len(people))
-print("soph" in people)
-print(people[:1])
+#print(people[0])
+#print(people.index("savannah"))
+#print(len(people))
+#("soph" in people)
+#(people[:1])
 
 #Dictionaries
 #Allow you to create key value pairs
 dog = {"name": "Roger"}
-print(dog["name"])
-print(dog.get("name"))
-print(dog.get("color", "brown"))
+#print(dog["name"])
+#print(dog.get("name"))
+#print(dog.get("color", "brown"))
 dog.pop("name")
-print(dog.keys())
-print(list(dog.values()))
-print(dog.items())
+#print(dog.keys())
+#print(list(dog.values()))
+#print(dog.items())
 dog["favourite food"] = "meat"
 print(dog)
 #del dog["color"]
 dogCopy = dog.copy()
-print(dogCopy)
+
+#print(dogCopy)
 
 
 #closures
@@ -234,9 +237,9 @@ def counter():
 
 increment = counter()
 
-print(increment())
-print(increment())
-print(increment())
+#print(increment())
+#print(increment())
+#print(increment())
 
 #loops
 #in python, there are two kinds of loops, while loops and for loops
@@ -289,7 +292,7 @@ for computer in computers:
 class Animal:
 
   def walk(self):
-    print('walking....')
+    return 'walking....'
 
 
 class Dog(Animal):
@@ -302,20 +305,20 @@ class Dog(Animal):
     return True if self.age > other.age else False
 
   def bark(self):
-    print("woof!!")
+    return "woof!!"
 
 
 #create an instance of a class
 roger = Dog("roger", 12)
 captain = Dog("captain", 15)
-print(roger > captain)
-print(type(roger))
+#print(roger > captain)
+#print(type(roger))
 
-print(roger.age)
-print(roger.name)
+#print(roger.age)
+#print(roger.name)
 
-roger.bark()
-roger.walk()
+#roger.bark()
+#roger.walk()
 
 #modules
 #Every python file is a module
@@ -330,7 +333,8 @@ lambda a, b: a * b
 
 #lambda functions cannot be invoked directly but can be assigned to a variable eg
 add = lambda a, b: a + b
-print(add(56, 67))
+
+#print(add(56, 67))
 
 
 #map(),filter() and reduce
@@ -341,7 +345,7 @@ def myFunc(n):
 
 myList = ('apple', 'banana', 'cherry')
 x = map(myFunc, myList)
-print(list(x))
+#print(list(x))
 
 #double the numbers in a list using the map() function
 #we can also put the lambda function straight into the map()
@@ -349,21 +353,22 @@ numbers = [1, 2, 3, 4, 5, 6]
 double = lambda n: n * 2
 
 result = map(double, numbers)
-print(list(result))
+#print(list(result))
 
 #The filter method
 #The filter method takes in an iterable and returns a filtered object which is also another iterable but without some of the original items
 isEven = lambda n: n % 2 == 0
 
 even_numbers = filter(isEven, numbers)
-print(list(even_numbers))
+#print(list(even_numbers))
 
 #Reduce
 #we have to import reduce from functools
 expenses = [("dinner", 80), ("bills", 50)]
 
 total_expenses = reduce(lambda a, b: a[1] + b[1], expenses)
-print(total_expenses)
+
+#print(total_expenses)
 
 
 #Recursion
@@ -373,7 +378,7 @@ def factorial(n):
   return n * factorial(n - 1)
 
 
-print(factorial(3))
+#print(factorial(3))
 
 #Decorators
 #decorators are used to alter how a function works. we use the @ followed by the decorator name
@@ -417,7 +422,7 @@ def increment(n: int) -> int:
   return n + 1
 
 
-print(increment(7))
+#print(increment(7))
 
 #Exception handling
 #for exception handling in python, we wrap lines of code in a try block and then python will alert us on which error occured using an except block
@@ -432,7 +437,7 @@ except ZeroDivisionError:
 finally:
   result = 1
 
-print(result)
+#print(result)
 
 #we can also raise an exception intentionally by using the raise key word
 #raise Exception("An Error occured!!")
@@ -457,77 +462,76 @@ except DogNotFoundException:
 #List compression is a way to create lists in a very concise way
 #eg we can compose an elements list elements compressed to the power of two
 #list compressions are mostly preferred over loops and are more useful when an operation can be written on a single line
-print("list compression")
+#print("list compression")
 nums = [1, 2, 3, 4, 5, 6, 7]
 numsquare = [n**2 for n in nums]
-print(numsquare)
+#print(numsquare)
 
 #the above code would be the same as
 sq = []
 for num in nums:
   sq.append(num**2)
 
-print(sq)
+#print(sq)
 
 #intermediate python
-mylist=["ok"]
+mylist = ["ok"]
 #insert at a specific index
-mylist.insert(0,"no")
-print(mylist)
+mylist.insert(0, "no")
+#print(mylist)
 #empty a list
 mylist.clear()
-print(mylist)
+#print(mylist)
 
-newlist=[0]*5
-print(newlist)
+newlist = [0] * 5
+#print(newlist)
 
-fakelist=[1,2,3,4,5,7,8]
-slicedlist=fakelist[2:5]
-steplist=fakelist[::2] #step index returns every second item
-print(steplist)
-print(slicedlist)
+fakelist = [1, 2, 3, 4, 5, 7, 8]
+slicedlist = fakelist[2:5]
+steplist = fakelist[::2]  #step index returns every second item
+#print(steplist)
+#print(slicedlist)
 
 #copy elements of a list
-list_org=["banana","cherry","appple"]
+list_org = ["banana", "cherry", "appple"]
 #list_copy=list(list_org)
 #list_copy=list_org[:]
-list_copy=list_org.copy()
+list_copy = list_org.copy()
 
 #Tuples
 #collection datatype that is ordered and immutable
 #cannot be changed and modified after its creation
 #a tuple is usef for objects that belong together
 #brackets can be optional
-my_tuple=("Max",28,"Boston")
-print(my_tuple)
+my_tuple = ("Max", 28, "Boston")
+#print(my_tuple)
 
 #iterate over a tuple using a for in loop
 #check if an element exists in a tuple
-val="yes" if "Max" in my_tuple else "no"
-print(val)
+val = "yes" if "Max" in my_tuple else "no"
+#print(val)
 #unpack elements of a tuple
-name,age,city=my_tuple
-print(city)
+name, age, city = my_tuple
+#print(city)
 
-number_tuple=(0,1,2,3,4)
-i1,*i2,i3=number_tuple
-print(i2)
+number_tuple = (0, 1, 2, 3, 4)
+i1, *i2, i3 = number_tuple
+#print(i2)
 
 #dictionaries
 #del mydict["key"]
 #pop()
 
 #merge two dictionaries
-dict1={"name":"sophia","age":27,"email":"sally@gmail.com"}
-dict2=dict(name="Mary",age=28,city="Boston")
+dict1 = {"name": "sophia", "age": 27, "email": "sally@gmail.com"}
+dict2 = dict(name="Mary", age=28, city="Boston")
 dict1.update(dict2)
-print(dict1)
+#print(dict1)
 
 #we can use other immuable types like integers and tuples as keys in a dictionary. lists cannot be used
 
-
 #sets
-myset=set()
+myset = set()
 
 #add to set
 myset.add(1)
@@ -540,26 +544,26 @@ myset.add(1)
 
 #union and intersection
 #union combine elements from two sets without duplication
-odds={1,3,5,7,9}
-even={0,2,4,6,8}
-primes={2,3,5,7}
-u=odds.union(even)
-print(u)
+odds = {1, 3, 5, 7, 9}
+even = {0, 2, 4, 6, 8}
+primes = {2, 3, 5, 7}
+u = odds.union(even)
+#print(u)
 
 #intersections
-i=odds.intersection(primes)
-print(i)
+i = odds.intersection(primes)
+#print(i)
 
 #difference
-diff=odds.difference(primes)
+diff = odds.difference(primes)
 
 #symmetric difference
-symdiff=odds.symmetric_difference(primes)
-print(symdiff)
+symdiff = odds.symmetric_difference(primes)
+#print(symdiff)
 
-merge=odds.update(primes)
-print(merge)
-print(diff)
+merge = odds.update(primes)
+#print(merge)
+#print(diff)
 
 #other methods
 #intersection_update
@@ -567,4 +571,74 @@ print(diff)
 #symmetric_difference_update
 
 #frozenset is an immutablle version of a set
-a=frozenset([1,2,3,4,5,6])
+a = frozenset([1, 2, 3, 4, 5, 6])
+
+#strings
+mystring = "Hello World"
+slicedString = mystring[1:4]
+#print(slicedString)
+
+#iterate over a string using a for in loop
+#check for existence of a character using an if in statement
+
+#remove white space
+spacedString = "    Hello  "
+spacedString = spacedString.strip()  #removes white space from a string
+
+#other methods
+#startsWith()
+#endsWith()
+#.find('string') returns thefirst index where the string exists
+#.count()
+#.replace() we can replace a substring
+#.split(delimiter)
+
+greeting = 'hello,how,are,you'
+newGreeting = greeting.split(',')
+#print(newGreeting)
+
+joinedGreeting = " ".join(newGreeting)
+#print(joinedGreeting)
+
+duplicatedAs = ['a'] * 6
+joinedAs = "".join(duplicatedAs)
+#print(joinedAs)
+
+#formatting strings
+#%,format(),f strings
+
+variable = 4.7824
+variable2 = 6
+variableString = "The variable is {:.2f} and {}".format(variable, variable2)
+#print(variableString)
+
+#using f strings
+fstring = f"The variables are {variable*2} and {variable2}"
+#print(fstring)
+
+#collections
+#The collections module implements special container data types and provides alternatives with some additional functionality compared to the general built in containers like dictionaries, lists or tuples
+#These include;
+#Counter
+#namedTuple
+#OrderedDict
+#defaultDict
+#deque
+
+from collections import Counter
+#The counter is a container that stores the elements as dictionary keys and their count as dictionary values
+a = "aaaaabbbbddhskhfg"
+my_counter = Counter(a)
+#print(my_counter)
+#print(my_counter.items())
+#print(my_counter.keys())
+#print(my_counter.values())
+print(my_counter.most_common(2))  #prints the commonest occurences
+print(
+  my_counter.most_common(1)[0][0])  #prints the commonest element which is a
+print(list(
+  my_counter.elements()))  #gives us an iterable that we can convert to a list
+
+#namedtuple
+#The named tuple is an easy to create light weight object type similar to a struct
+from collections import namedtuples
